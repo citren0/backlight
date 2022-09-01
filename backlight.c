67348391bfa95a8a5163cd7db39c1884d8388d3a
Citren0 <alphahak22@gmail.com>
2022-08-31 21:11:19 -0600

    added source file

---
+++ backlight.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+  The purpose of this program is to set the backlight on my laptop
+  which refuses to work with xbacklight.
+*/
+
+/*
+  Constants, should be changed to suit your system.
+*/
+const int maxBrightness = 512;
+const int minBrightness = 0;
+const char* fileLocation = "/sys/class/backlight/intel_backlight/brightness";
+
+/*
+  Prototypes
+*/
+void usage(char* argv[], int max, int min);
+
+int main(int argc, char* argv[]) {
+    if (argc < 2 || argc > 2 || atoi(argv[1]) > maxBrightness || atoi(argv[1]) < minBrightness) {
+        usage(argv, maxBrightness, minBrightness);
+    }
+
+    FILE *fp;
+    fp = fopen(fileLocation, "w");
+
+    if (!fp) {
+        printf("Backlight control file not found. Run with sudo, run as root, or modify file path in source.\n");
+        exit(1);
+    }
+
+    fprintf(fp, argv[1]);
+    exit(0);
+}
+
+
+void usage(char* argv[], int max, int min) {
+    printf("Usage: # %s %d-%d\n", argv[0], min, max);
+    exit(1);
+}
