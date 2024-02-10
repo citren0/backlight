5b4b11078cc693492ee9e450db4c6939a4fd22c6
Cameron D <42524057+citren0@users.noreply.github.com>
2024-02-10 04:40:27 +0000

    Further fixed styling and returning in main()

--- backlight.c
+++ backlight.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 // The purpose of this program is to set the backlight on my laptop which refuses to work with xbacklight.
 
 // Constants, should be changed to suit your system.
@@ -10,30 +9,30 @@ const int minBrightness = 0;
 const char * fileLocation = "/sys/class/backlight/intel_backlight/brightness";
 
 // Prototype
-void usage(char* argv[], int max, int min);
+void usage(char * argv[], int max, int min);
 
-int main(int argc, char* argv[])
+int main(int argc, char * argv[])
 {
     if ((argc < 2) || (argc > 2) || (atoi(argv[1]) > maxBrightness) || (atoi(argv[1]) < minBrightness))
     {
         usage(argv, maxBrightness, minBrightness);
+        return 1;
     }
 
-    FILE *fp;
+    FILE * fp;
     fp = fopen(fileLocation, "w");
 
     if (!fp)
     {
         printf("Backlight control file not found. Run with sudo, run as root, or modify file path in source.\n");
-        exit(1);
+        return 1;
     }
 
     fprintf(fp, argv[1]);
-    exit(0);
+    return 0;
 }
 
 void usage(char* argv[], int max, int min)
 {
     printf("Usage: # %s %d-%d\n", argv[0], min, max);
-    exit(1);
 }
